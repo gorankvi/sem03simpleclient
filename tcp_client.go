c951f750f09751a5115ce032677a9d0aa81c0599
gorankvi <goran.kvitne@gmail.com>
2023-04-25 15:53:49 +0000

    siste endring

--- tcp_client.go
+++ tcp_client.go
@@ -4,17 +4,20 @@ import (
 	"net"
 	"log"
 	"os"
+	"github.com/gorankvi/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.3:8080")
+	conn, err := net.Dial("tcp", "172.17.0.4:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+ 	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +25,7 @@ func main() {
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	}
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Printf("reply from proxy: %s", string(dekryptertMelding))
 }
